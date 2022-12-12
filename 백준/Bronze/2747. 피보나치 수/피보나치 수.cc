f36e1ffcbbe290b74ffc9353a79c194b60a69349
uoise <81301090+uoise@users.noreply.github.com>
2022-12-12 23:32:51 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+using ll = long long;
+ll dp[45];
+ll fib(int n) {
+  if (n <= 1) return n;
+  if (dp[n]) return dp[n];
+  return dp[n] = fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  printf("%lld", fib(n));
+}
