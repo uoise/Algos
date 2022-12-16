1d384d7f1c0f1e81a7a17ca793fbf7ad80f22ca1
uoise <81301090+uoise@users.noreply.github.com>
2022-12-17 02:33:53 +0900

    [Gold IV] Title: 전력난, Time: 340 ms, Memory: 4240 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.cc
+++ 백준/Gold/6497. 전력난/전력난.cc
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <cstdio>
-#include <vector>
 
 using namespace std;
 struct xyz {
@@ -10,7 +9,10 @@ struct xyz {
   }
 };
 
-int n, m, bv[200'000];
+constexpr int siz = 200'000;
+int n, m, bv[siz];
+xyz vv[siz];
+
 int fnd(int x) {
   if (bv[x] == x) return x;
   return bv[x] = fnd(bv[x]);
@@ -23,17 +25,15 @@ int mks(int x, int y) {
 }
 
 int sol() {
-  vector<xyz> vv(n);
   int res = 0, sum = 0, cnt = 0;
   for (int i = 0; i < m; i++) bv[i] = i;
-  for (auto& p : vv) {
-    scanf("%d %d %d", &p.x, &p.y, &p.z);
-    res += p.z;
+  for (int i = 0; i < n; i++) {
+    scanf("%d %d %d", &vv[i].x, &vv[i].y, &vv[i].z);
+    res += vv[i].z;
   }
-
+    
   if (n == m - 1) return 0;
-
-  sort(vv.begin(), vv.end());
+  sort(vv, vv + n);
   for (const auto& p : vv) {
     if (mks(p.x, p.y)) {
       sum += p.z;
