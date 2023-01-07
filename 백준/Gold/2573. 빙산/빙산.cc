a07a93c75beed1abe4f71905c357d6f12635c7c0
uoise <81301090+uoise@users.noreply.github.com>
2023-01-07 17:50:35 +0900

    [Gold IV] Title: 빙산, Time: 60 ms, Memory: 2408 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -1,10 +1,10 @@
 #include <cstdio>
 #include <cstring>
 
-constexpr int xx[] = {1, 0, -1, 0}, yy[] = {0, 1, 0, -1};
 int n, m, t, vv[300][300], nv[300][300];
 bool bv[300][300];
-constexpr auto sz = sizeof(bv), cz = sizeof(nv);
+constexpr int xx[] = {1, 0, -1, 0}, yy[] = {0, 1, 0, -1};
+constexpr auto sz = sizeof bv, cz = sizeof nv;
 void dfs(int y, int x) {
   int cnt = 0;
   bv[y][x] = 1;
@@ -21,7 +21,6 @@ void dfs(int y, int x) {
 }
 
 int sol() {
-  int ny, nx;
   while (1) {
     int cnt = 0;
     for (int y = 0; y < n; y++)
@@ -30,7 +29,6 @@ int sol() {
     if (cnt != 1) return cnt ? t : 0;
     memcpy(vv, nv, cz);
     memset(bv, 0, sz);
-    memset(nv, 0, cz);
     t++;
   }
 }
@@ -39,6 +37,5 @@ int main() {
   scanf("%d %d", &n, &m);
   for (int y = 0; y < n; y++)
     for (int x = 0; x < m; x++) scanf("%d", &vv[y][x]);
-
   printf("%d", sol());
 }
