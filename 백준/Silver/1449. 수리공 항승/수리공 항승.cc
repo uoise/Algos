b636a4d4706fa58fe7a48a959bd741cbef4ec201
uoise <81301090+uoise@users.noreply.github.com>
2023-01-31 21:14:44 +0900

    [Silver III] Title: 수리공 항승, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+int n, l, v[1001], x, r;
+int main() {
+  scanf("%d %d", &n, &l);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &x);
+    v[x] = 1;
+  }
+  x = 0;
+  for (int i = 0; i < 1001; i++) {
+    if (x) --x;
+    if (v[i] && !x) x = l, ++r;
+  }
+  printf("%d", r);
+}
