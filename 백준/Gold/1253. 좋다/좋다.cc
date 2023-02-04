9fd3a7b3d86590259f95ef1efc882c6a2d2f8b75
uoise <81301090+uoise@users.noreply.github.com>
2023-02-04 15:41:05 +0900

    [Gold IV] Title: 좋다, Time: 20 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -3,23 +3,26 @@
 
 using namespace std;
 
-int n, v[2000], r, x;
-pair<int, int> s[2000 * 2000];
-bool sol(const int i) {
-  auto b = lower_bound(s, s + x, make_pair(v[i], 0));
-  auto e = lower_bound(s, s + x, make_pair(v[i] + 1, 0));
-  for (b; b != e; b++)
-    if (b->second / n != i && b->second % n != i) return 1;
-  return 0;
-}
+int n, v[2000], b, e, t, r;
 
 int main() {
   scanf("%d", &n);
   for (int i = 0; i < n; i++) scanf("%d", &v[i]);
-  for (int i = 0; i < n; i++)
-    for (int j = i + 1; j < n; j++) s[x++] = {v[i] + v[j], i * n + j};
-  sort(s, s + x);
-  for (int i = 0; i < n; i++)
-    if (sol(i)) ++r;
+  sort(v, v + n);
+  for (int i = 0; i < n; i++) {
+    b = 0, e = n - 1;
+    while (b < e) {
+      if (b == i) ++b;
+      if (e == i) --e;
+      if(b >= e) break;
+      t = v[b] + v[e];
+      if (t == v[i]) {
+        ++r;
+        break;
+      }
+      if (t < v[i]) ++b;
+      else --e;
+    }
+  }
   printf("%d", r);
 }
