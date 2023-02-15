821257c02ec0ff69f8072f33da5cc6147bfca3b8
uoise <81301090+uoise@users.noreply.github.com>
2023-02-15 14:22:30 +0900

    [Gold IV] Title: 공유기 설치, Time: 44 ms, Memory: 1896 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -1,32 +1,31 @@
 #include <algorithm>
-#include <iostream>
+#include <cstdio>
+int n, c, v[200001], r, l;
 
-int n, c, v[200001];
-int chk(const int& lgt) {
-  int res = 1, lst = 0;
+bool clc(const int& len) {
+  r = 1, l = 0;
   for (int i = 1; i < n; i++)
-    if (v[i] - v[lst] >= lgt) {
-      res++;
-      lst = i;
-    }
-  return res;
+    if (v[i] - v[l] >= len) ++r, l = i;
+  return r >= c;
 }
-int solve() {
-  int res, val, beg = 0, end = v[n - 1];
-  while (beg + 1 < end) {
-    res = (beg + end) / 2;
-    val = chk(res);
-    if (val >= c && chk(res + 1) < c) break;
-    if (val < c)
-      end = res;
-    else
-      beg = res;
+
+int sol() {
+  int beg = 0, end = v[n - 1], mid, res;
+  while (beg <= end) {
+    mid = (beg + end) / 2;
+    if (clc(mid)) {
+      res = mid;
+      beg = mid + 1;
+    } else {
+      end = mid - 1;
+    }
   }
   return res;
 }
+
 int main() {
   scanf("%d %d", &n, &c);
   for (int i = 0; i < n; i++) scanf("%d", &v[i]);
   std::sort(v, v + n);
-  printf("%d\n", solve());
+  printf("%d", sol());
 }
