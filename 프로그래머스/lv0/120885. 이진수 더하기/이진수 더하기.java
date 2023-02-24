38eb683818c3ef7de9a2985d9e3035fb1c8293e5
uoise <81301090+uoise@users.noreply.github.com>
2023-02-24 13:45:36 +0900

    [level 0] Title: 이진수 더하기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        long tmp = Long.parseLong(bin1) + Long.parseLong(bin2);
-        if (tmp == 0) return "0";
-        StringBuilder res = new StringBuilder();
-        while (tmp > 0) {
-            if (tmp % 10 > 1) tmp += 8;
-            res.append(tmp % 10);
-            tmp /= 10;
-        }
-        return res.reverse().toString();
+        return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2), 2);
     }
 }
