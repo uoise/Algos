208c55a3e60f78ddd1f2e018400700b5c621c4f5
uoise <81301090+uoise@users.noreply.github.com>
2023-02-28 11:04:03 +0900

    [Bronze V] Title: 꼬마 정민, Time: 128 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,8 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-        System.out.println(Arrays.stream(new Scanner(System.in).nextLine().split(" ")).mapToLong(Long::parseLong).sum());
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.stream(new BufferedReader(new InputStreamReader(System.in)).readLine().split(" ")).mapToLong(Long::parseLong).sum());
     }
 }
