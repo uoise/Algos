02c0557122f614c32c8940878471a14b3af11f90
uoise <81301090+uoise@users.noreply.github.com>
2023-03-03 10:24:41 +0900

    [level 0] Title: 암호 해독, Time: 5.61 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,7 +1,11 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Solution {
     public String solution(String cipher, int code) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = code - 1; i < cipher.length(); i += code) sb.append(cipher.charAt(i));
-        return sb.toString();
+        return IntStream.range(0, cipher.length())
+                .filter(i -> (i + 1) % code == 0)
+                .mapToObj(i -> String.valueOf(cipher.charAt(i)))
+                .collect(Collectors.joining());
     }
 }
