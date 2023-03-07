9c07070b829c750780f302c2a2158411ba3f55c9
uoise <81301090+uoise@users.noreply.github.com>
2023-03-07 10:48:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 30.72 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        return IntStream.range(0, arr.length).filter(i -> i == 0 || arr[i] != arr[i - 1]).map(i -> arr[i]).toArray();
+    }
+}
