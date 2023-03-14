70aa99bb58cdbe549d52da01a5a877d43c2938e8
uoise <81301090+uoise@users.noreply.github.com>
2023-03-14 18:39:09 +0900

    [Silver V] Title: 피보나치 수 4, Time: 256 ms, Memory: 24940 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -1,34 +1,16 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 class Main {
-    public static StringBuilder ret;
-    public static int res;
-
-    public static String add(String x, String y) {
-        res = 0;
-        ret = new StringBuilder();
-        for (int i = 0; i < Math.max(x.length(), y.length()); i++) {
-            if (i < x.length()) res += (x.charAt(x.length() - i - 1) - '0');
-            if (i < y.length()) res += (y.charAt(y.length() - i - 1) - '0');
-            ret.append(res % 10);
-            res /= 10;
-        }
-        if (res > 0) ret.append(res);
-        return ret.reverse().toString();
-    }
-
     public static String sol() {
         int n = new Scanner(System.in).nextInt();
-        if (n <= 1) return String.valueOf(n);
-        String h = "0";
-        String p = "1";
-        String c = "";
-        for (int i = 2; i <= n; i++) {
-            c = add(h, p);
-            h = p;
-            p = c;
-        }
-        return c;
+        if (n < 2) return String.valueOf(n);
+        BigInteger[] dp = new BigInteger[n + 1];
+        dp[0] = BigInteger.ZERO;
+        dp[1] = BigInteger.ONE;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1].add(dp[i - 2]);
+        return dp[n].toString();
     }
 
     public static void main(String[] args) {
