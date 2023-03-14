c4ded36ec14a44584da85276995e9af5527cbc35
uoise <81301090+uoise@users.noreply.github.com>
2023-03-14 17:22:23 +0900

    [level 2] Title: 숫자의 표현, Time: 3.16 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/README.md
+++ 프로그래머스/lv2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924#) 
 
 ### 성능 요약
 
-메모리: 52.2 MB, 시간: 1.55 ms
+메모리: 52.8 MB, 시간: 3.16 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,16 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n) {
-        int answer = 1, sum;
-        for (int i = 1; i < n; i++) {
-            sum = 0;
-            for (int j = i; j <= n && sum < n; j++) {
-                sum += j;
-                if (sum == n) {
-                    answer++;
-                    break;
-                }
-            }
-        }
-        return answer;
+        return (int) IntStream.rangeClosed(1, n).filter(i -> n % i == 0 && i % 2 == 1).count();
     }
 }
