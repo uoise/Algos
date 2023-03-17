24c6d2b542f8a207833bf5c137e2b503862a887e
uoise <81301090+uoise@users.noreply.github.com>
2023-03-17 17:48:01 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while (n > 0) {
+            if (n % 2 == 1) ans++;
+            n >>= 1;
+        }
+        return ans;
+    }
+}
