18b4a2253c4e533ee01ec3ac34b8779d76b90fcb
uoise <81301090+uoise@users.noreply.github.com>
2023-03-20 17:24:17 +0900

    [level 1] Title: 실패율, Time: 4.53 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[][] ar = new int[N + 1][3];
+        for (int[] v : ar) Arrays.fill(v, 0);
+        for (int i = 0; i < ar.length; i++) {
+            ar[i][0] = i;
+            ar[0][1] = 0;
+            ar[0][2] = 0;
+        }
+        for (int usr : stages) {
+            for (int i = 0; i <= Math.min(usr, N); i++) {
+                ar[i][1]++;
+                ar[i][2]++;
+            }
+            if (usr <= N) ar[usr][1]--;
+        }
+        Arrays.sort(ar, Comparator.comparingDouble(x -> -1d * (x[2] - x[1]) / x[2]));
+        return Arrays.stream(ar).mapToInt(v -> v[0]).filter(i -> i > 0).toArray();
+    }
+}
