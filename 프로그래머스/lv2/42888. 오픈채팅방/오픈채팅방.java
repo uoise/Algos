8d97ab1848b7413b372ff34062768d5091fcd8ec
uoise <81301090+uoise@users.noreply.github.com>
2023-04-06 11:37:44 +0900

    [level 2] Title: 오픈채팅방, Time: 87.77 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    class Sb {
+        String s;
+        boolean b;
+
+        public Sb(String s, boolean b) {
+            this.s = s;
+            this.b = b;
+        }
+    }
+
+    public String[] solution(String[] record) {
+        Map<String, String> map = new HashMap<>();
+        Queue<Sb> q = new LinkedList<>();
+        StringTokenizer st;
+        for (String s : record) {
+            st = new StringTokenizer(s);
+            String op = st.nextToken();
+            String uid = st.nextToken();
+            if (st.hasMoreTokens()) {
+                map.put(uid, st.nextToken());
+                if (op.equals("Enter")) {
+                    q.add(new Sb(uid, true));
+                }
+            } else {
+                q.add(new Sb(uid, false));
+            }
+        }
+        String[] answer = new String[q.size()];
+        for (int i = 0; i < answer.length; i++) {
+            Sb cur = q.poll();
+            answer[i] = map.get(cur.s) + (cur.b ? "님이 들어왔습니다." : "님이 나갔습니다.");
+        }
+        return answer;
+    }
+}
