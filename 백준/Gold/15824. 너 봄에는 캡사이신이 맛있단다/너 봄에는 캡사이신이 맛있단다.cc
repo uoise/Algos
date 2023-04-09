d8ea3ef4f28b2dcd30eb69768441ec5150c8a32e
uoise <81301090+uoise@users.noreply.github.com>
2023-04-09 22:56:23 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 208 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cstdio>
+
+using ll = long long;
+
+constexpr ll MX = 1e9 + 7;
+int n, v[300000];
+ll pow(int e) {
+  if (!e) return 1;
+  if (e & 1) return 2L * pow(e - 1) % MX;
+  ll r = pow(e / 2) % MX;
+  return r * r % MX;
+}
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) scanf("%d", &v[i]);
+  std::sort(v, v + n);
+  ll res = 0;
+  for (int i = 0; i < n; i++)
+    res = (res + v[i] * (pow(i) - pow(n - 1 - i))) % MX;
+  printf("%d", res);
+}
