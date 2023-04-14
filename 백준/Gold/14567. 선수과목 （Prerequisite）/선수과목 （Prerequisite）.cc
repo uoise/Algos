49698f48048ff0d153ddce4e5ca571166a9d03ba
uoise <81301090+uoise@users.noreply.github.com>
2023-04-14 17:24:47 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 132 ms, Memory: 3692 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -1,18 +1,16 @@
 #include <cstdio>
 #include <queue>
-#include <unordered_set>
 
 constexpr int MX = 1001;
 
-std::unordered_set<int> sv[MX];
-std::queue<int> q;
+std::queue<int> q, sq[MX];
 int n, m, u, v, r, rv[MX], vv[MX];
 int main() {
   scanf("%d %d", &n, &m);
   for (int i = 0; i < m; i++) {
     scanf("%d %d", &u, &v);
     vv[v]++;
-    sv[u].insert(v);
+    sq[u].push(v);
   }
 
   while (++r) {
@@ -26,7 +24,10 @@ int main() {
     while (!q.empty()) {
       int c = q.front();
       q.pop();
-      for (auto it = sv[c].begin(); it != sv[c].end(); it++) vv[*it]--;
+      while (!sq[c].empty()) {
+        vv[sq[c].front()]--;
+        sq[c].pop();
+      }
     }
   }
   for (int i = 1; i <= n; i++) printf("%d ", rv[i]);
