8b91eb50d9f81a9f0be6782e602f5da037686099
uoise <81301090+uoise@users.noreply.github.com>
2023-04-18 20:13:14 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,24 +1,28 @@
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    private void dfs(int cur, boolean[] bv, int[][] vv) {
-        for (int i = 0; i < vv[cur].length; i++) {
-            if (bv[i] || vv[cur][i] == 0) continue;
-            bv[i] = true;
-            dfs(i, bv, vv);
-        }
+    static int fnd(int x, int[] arr) {
+        if (arr[x] == x) return x;
+        return arr[x] = fnd(arr[x], arr);
+    }
+
+    static boolean union(int x, int y, int[] arr) {
+        x = fnd(x, arr);
+        y = fnd(y, arr);
+        if (x == y) return false;
+        arr[Math.max(x, y)] = Math.min(x, y);
+        return true;
     }
 
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] bv = new boolean[n];
-        Arrays.fill(bv, false);
-        for (int i = 0; i < n; i++) {
-            if (!bv[i]) {
-                dfs(i, bv, computers);
-                ++answer;
-            }
-        }
-        return answer;
+        int[] group = new int[n];
+        for (int i = 0; i < n; i++) group[i] = i;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (i != j && computers[i][j] == 1) union(i, j, group);
+        Set<Integer> answer = new HashSet<>();
+        for (int g : group) answer.add(fnd(g, group));
+        return answer.size();
     }
 }
