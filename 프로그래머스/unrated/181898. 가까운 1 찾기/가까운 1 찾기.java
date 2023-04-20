448d03b1bc1fc78c21daf40ed455fedbb35095f1
uoise <81301090+uoise@users.noreply.github.com>
2023-04-20 15:39:02 +0900

    [unrated] Title: 가까운 1 찾기, Time: 1.84 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] arr, int idx) {
+        return IntStream.range(idx, arr.length).filter(i -> arr[i] == 1).findFirst().orElse(-1);
+    }
+}
