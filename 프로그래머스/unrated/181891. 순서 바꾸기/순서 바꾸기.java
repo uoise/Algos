f58abcd2b80a437d2f6373fad9d085acd3d2f6b6
uoise <81301090+uoise@users.noreply.github.com>
2023-04-20 14:56:15 +0900

    [unrated] Title: 순서 바꾸기, Time: 3.03 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return IntStream.range(0, num_list.length).map(i -> num_list[(i + n) % num_list.length]).toArray();
+    }
+}
