a2c835c9887ba449695deb35ec7741a1de19cef6
uoise <81301090+uoise@users.noreply.github.com>
2023-05-11 16:38:53 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        for (int i = 0; i < db.length; i++) {
-            if (id_pw[0].equals(db[i][0])) {
-                if (id_pw[1].equals(db[i][1])) return "login";
+        for (String[] strings : db) {
+            if (id_pw[0].equals(strings[0])) {
+                if (id_pw[1].equals(strings[1])) return "login";
                 else return "wrong pw";
             }
         }
