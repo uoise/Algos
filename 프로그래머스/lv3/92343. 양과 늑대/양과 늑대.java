fe251eb2d6af2f2302959218d55788d41e9ab3b8
uoise <81301090+uoise@users.noreply.github.com>
2023-05-16 11:12:07 +0900

    [level 3] Title: 양과 늑대, Time: 0.51 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,31 @@
+class Solution {
+    boolean[][] vv;
+    int siz, answer = 0;
+
+    void dfs(final int cur, final int shp, final int wlf, boolean[] bv) {
+        boolean[] nv = bv.clone();
+        nv[cur] = false;
+        if (shp <= wlf) return;
+        answer = Math.max(answer, shp);
+        for (int i = 0; i < vv.length; i++) {
+            if (cur == i || !vv[cur][i]) continue;
+            nv[i] = true;
+        }
+
+        for (int i = 0; i < bv.length; i++) {
+            if (!nv[i]) continue;
+            nv[i] = false;
+            dfs(i, shp + (vv[i][i] ? 1 : 0), wlf + (vv[i][i] ? 0 : 1), nv);
+            nv[i] = true;
+        }
+    }
+
+    public int solution(int[] info, int[][] edges) {
+        siz = info.length;
+        vv = new boolean[siz][siz];
+        for (int i = 0; i < info.length; i++) vv[i][i] = info[i] == 0;
+        for (int[] e : edges) vv[e[0]][e[1]] = true;
+        dfs(0, (vv[0][0] ? 1 : 0), (vv[0][0] ? 0 : 1), vv[0]);
+        return answer;
+    }
+}
