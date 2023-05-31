67ce3754197792ebe93661e845a94a3de7c5ddb0
uoise <81301090+uoise@users.noreply.github.com>
2023-05-31 22:05:55 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 548 ms, Memory: 20788 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <queue>
+#include <vector>
+
+std::vector<std::vector<int>> bv;
+std::vector<int> dv;
+std::queue<int> q;
+int n, m, k, x, s, e;
+int main() {
+  scanf("%d %d %d %d", &n, &m, &k, &x);
+  bv.assign(n + 1, std::vector<int>());
+  dv.assign(n + 1, 0);
+  while (m--) {
+    scanf("%d %d", &s, &e);
+    bv[s].push_back(e);
+  }
+  ++k;
+  dv[x] = 1;
+  q.push(x);
+  while (!q.empty()) {
+    x = q.front();
+    q.pop();
+    for (const int& nxt : bv[x]) {
+      if (dv[nxt]) continue;
+      dv[nxt] = dv[x] + 1;
+      q.push(nxt);
+    }
+  }
+  m = 0;
+  for (int i = 1; i <= n; i++)
+    if (dv[i] == k) {
+      printf("%d\n", i);
+      m = 1;
+    }
+  if (!m) printf("-1");
+}
