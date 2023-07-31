c263ea9f315e18b2a84fff9f8287117d4a299134
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-07-31 18:54:22 +0900

    [Gold V] Title: 인내의 도미노 장인 호석, Time: 8 ms, Memory: 1280 KB -BaekjoonHub

---
+++ 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.cc
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cstdio>
+#include <unordered_map>
+
+constexpr int MX = 101, xx[] = {1, 0, -1, 0}, yy[] = {0, 1, 0, -1};
+std::unordered_map<char, int> dm;
+int n, m, vv[MX][MX];
+bool bv[MX][MX];
+int psh(int y, int x, const int& d) {
+  int cnt = 0, lst = 1;
+  while (1) {
+    if (!bv[y][x]) lst = std::max(lst, vv[y][x]), ++cnt, bv[y][x] = 1;
+    if (!--lst) break;
+    y += yy[d], x += xx[d];
+    if (!y || y > n || !x || x > m) break;
+  }
+
+  return cnt;
+}
+
+int main() {
+  int x, y, r, s = 0;
+  char dc;
+  dm.insert({'E', 0});
+  dm.insert({'W', 2});
+  dm.insert({'S', 1});
+  dm.insert({'N', 3});
+  scanf("%d %d %d", &n, &m, &r);
+  for (y = 1; y <= n; y++)
+    for (x = 1; x <= m; x++) scanf("%d", &vv[y][x]);
+
+  while (r--) {
+    scanf("%d %d %c", &y, &x, &dc);
+    s += psh(y, x, dm[dc]);
+    scanf("%d %d", &y, &x);
+    bv[y][x] = 0;
+  }
+
+  printf("%d\n", s);
+  for (y = 1; y <= n; y++) {
+    for (x = 1; x <= m; x++) printf("%c ", bv[y][x] ? 'F' : 'S');
+    printf("\n");
+  }
+}
