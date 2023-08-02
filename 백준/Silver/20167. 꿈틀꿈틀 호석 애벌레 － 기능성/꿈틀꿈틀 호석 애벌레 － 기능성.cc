9369245147c878be582e9f4d4b75931ac4e45482
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-08-02 11:18:45 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cstdio>
+
+constexpr int MX = 21;
+int n, k, v[MX], m[MX], l, s;
+int main() {
+  scanf("%d %d", &n, &k);
+  for (int i = 0; i < n; i++) scanf("%d", &v[i]);
+  s = v[l];
+  for (int i = 1; i <= n; i++) {
+    m[i] = std::max(m[i - 1], m[l] + s - k);
+    while (s >= k) s -= v[l++];
+    s += v[i];
+  }
+
+  printf("%d", m[n]);
+}
