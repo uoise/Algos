4caab7c6eb3d1a4f92a9b1b7d5ce2c7288cf983a
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-08-18 12:18:18 +0900

    [Platinum V] Title: 버블 소트, Time: 160 ms, Memory: 5020 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -0,0 +1,48 @@
+#include <cstdio>
+
+constexpr int MX = 5e5 + 1;
+
+long long r;
+int n, v[MX], t[MX];
+void merge(const int& s, const int& e) {
+  const int m = (s + e) / 2;
+  int i = s, j = m + 1, k = s, cnt = 0;
+  while (i <= m && j <= e) {
+    if (v[i] <= v[j]) {
+      t[k++] = v[i++];
+      r += cnt;
+    } else {
+      t[k++] = v[j++];
+      ++cnt;
+    }
+  }
+
+  if (i > m) {
+    while (j <= e) {
+      t[k++] = v[j++];
+      ++cnt;
+    }
+  } else {
+    while (i <= m) {
+      t[k++] = v[i++];
+      r += cnt;
+    }
+  }
+
+  for (i = s; i <= e; i++) v[i] = t[i];
+}
+
+void sol(const int& s, const int& e) {
+  if (s >= e) return;
+  const int m = (s + e) / 2;
+  sol(s, m);
+  sol(m + 1, e);
+  merge(s, e);
+}
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) scanf("%d", &v[i]);
+  sol(0, n - 1);
+  printf("%lld", r);
+}
