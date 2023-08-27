0285c78a74e085eda3ca5203ef7c2baa9e5e3c36
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-08-27 23:46:40 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 1228 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <stack>
+
+std::stack<int> s;
+bool sol() {
+  int n, x, c = 1;
+  bool r = true;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &x);
+    if (x == c) {
+      ++c;
+      continue;
+    }
+
+    while (!s.empty() && c == s.top()) {
+      s.pop();
+      ++c;
+    }
+
+    s.push(x);
+  }
+
+  while (!s.empty() && c == s.top()) {
+    s.pop();
+    ++c;
+  }
+  
+  return s.empty();
+}
+
+int main() { printf("%s", sol() ? "Nice" : "Sad"); }
