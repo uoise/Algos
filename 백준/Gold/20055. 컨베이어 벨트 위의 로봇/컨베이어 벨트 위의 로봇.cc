f75903a507ff7b609e7b477b2b1a5c110534c599
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-09-02 19:03:33 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 32 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -3,39 +3,29 @@
 
 std::deque<int> q;
 bool v[101];
-int n, k, c, r;
-void sft() {
-  q.push_front(q.back());
-  q.pop_back();
-  for (int i = n - 2; i >= 0; i--) v[i + 1] = v[i];
-  v[0] = v[n - 1] = 0;
-}
-
-void mov() {
-  for (int i = n - 2; i; i--) {
-    if (!v[i] || v[i + 1] || !q[i + 1]) continue;
-    if (!--q[i + 1]) ++c;
-    v[i + 1] = i == n - 2 ? 0 : v[i];
-    v[i] = 0;
-  }
-}
-
-void clc() {
-  sft();
-  mov();
-  if (!q[0]) return;
-  v[0] = 1;
-  if (!--q[0]) ++c;
-}
-
+int n, k, c, r, x;
 int main() {
   scanf("%d %d", &n, &k);
-  int x;
   for (int i = 0; i < n * 2; i++) {
     scanf("%d", &x);
     q.push_back(x);
   }
 
-  while (c < k && ++r) clc();
+  while (c < k && ++r) {
+    q.push_front(q.back());
+    q.pop_back();
+    for (int i = n - 2; i >= 0; i--) v[i + 1] = v[i];
+    v[0] = v[n - 1] = 0;
+    for (int i = n - 2; i; i--) {
+      if (!v[i] || v[i + 1] || !q[i + 1]) continue;
+      if (!--q[i + 1]) ++c;
+      v[i + 1] = i == n - 2 ? 0 : v[i];
+      v[i] = 0;
+    }
+    if (!q[0]) continue;
+    v[0] = 1;
+    if (!--q[0]) ++c;
+  }
+  
   printf("%d", r);
 }
