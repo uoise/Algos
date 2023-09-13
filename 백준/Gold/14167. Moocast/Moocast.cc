1b86796ce47a936a325eeef0aaa74b871b09cc1d
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-09-13 12:58:51 +0900

    [Gold III] Title: Moocast, Time: 16 ms, Memory: 10468 KB -BaekjoonHub

---
+++ 백준/Gold/14167. Moocast/Moocast.cc
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <queue>
+
+struct cuv {
+  int c, u, v;
+  bool operator<(const cuv& o) const { return v > o.v; }
+};
+
+std::priority_queue<cuv> pq;
+int n, c, gv[1001], r, lv[1001][2], d;
+int fnd(const int& x) {
+  if (x == gv[x]) return x;
+  return gv[x] = fnd(gv[x]);
+}
+
+bool mks(int x, int y) {
+  x = fnd(x), y = fnd(y);
+  if (x == y) return false;
+  gv[x] = y;
+  return true;
+}
+
+int clc(const int& i, const int& j) {
+  int x = lv[i][0] - lv[j][0];
+  int y = lv[i][1] - lv[j][1];
+  return x * x + y * y;
+}
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d %d", &lv[i][0], &lv[i][1]);
+    gv[i] = i;
+  }
+
+  for (int i = 0; i < n; i++)
+    for (int j = i + 1; j <= n; j++) pq.push({i, j, clc(i, j)});
+
+  --n;
+  while (!pq.empty() && n) {
+    if (mks(pq.top().c, pq.top().u)) r = pq.top().v, --n;
+    pq.pop();
+  }
+
+  printf("%d", r);
+}
