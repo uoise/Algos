dace71b6314908c94e36679d0a665554abaab360
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-09-16 16:22:56 +0900

    [Silver III] Title: 님 무기가 좀 나쁘시네여, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/11387. 님 무기가 좀 나쁘시네여/님 무기가 좀 나쁘시네여.cc
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <cstdio>
+
+struct dam {
+  int att, str, icc, cdm, ias;
+  dam(int att, int str, int icc, int cdm, int ias) {
+    this->att = att, this->str = str;
+    this->icc = icc, this->cdm = cdm, this->ias = ias;
+  }
+
+  long long get() const {
+    long long cd = 100LL * (100 - std::min(icc, 100)) + std::min(icc, 100) * cdm;
+    return 1LL * att * (100 + str) * cd * (100 + ias);
+  }
+
+  dam operator+(const dam& o) const { return {att + o.att, str + o.str, icc + o.icc, cdm + o.cdm, ias + o.ias}; }
+  dam operator-(const dam& o) const { return {att - o.att, str - o.str, icc - o.icc, cdm - o.cdm, ias - o.ias}; }
+  bool operator<(const dam& o) const { return get() < o.get(); }
+  bool operator==(const dam& o) const { return get() == o.get(); }
+};
+
+int main() {
+  int att, str, icc, cdm, ias;
+  scanf("%d %d %d %d %d", &att, &str, &icc, &cdm, &ias);
+  dam cri = {att, str, icc, cdm, ias};
+
+  scanf("%d %d %d %d %d", &att, &str, &icc, &cdm, &ias);
+  dam pav = {att, str, icc, cdm, ias};
+
+  scanf("%d %d %d %d %d", &att, &str, &icc, &cdm, &ias);
+  dam crw = {att, str, icc, cdm, ias};
+
+  scanf("%d %d %d %d %d", &att, &str, &icc, &cdm, &ias);
+  dam pvw = {att, str, icc, cdm, ias};
+
+  dam crn = cri - crw + pvw, pvn = pav - pvw + crw;
+
+  printf("%c\n", cri == crn ? '0' : cri < crn ? '+' : '-');
+  printf("%c", pav == pvn ? '0' : pav < pvn ? '+' : '-');
+}
