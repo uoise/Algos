28298b85d38218400d6b14c711ecbb4650a23651
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-09-21 22:01:04 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.08 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,25 +1,36 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Iterator;
+import java.util.Stack;
 
 class Solution {
-    private static final Deque<Integer> stk = new ArrayDeque<>();
-    private static final int[] bgr = {1, 3, 2, 1};
+	static Stack<Integer> stk = new Stack<>(); // main stack
+	static Deque<Integer> ttk = new ArrayDeque<>(); // temporary stack
 
-    public int solution(int[] ingredient) {
-        int answer = 0;
-        for (int i : ingredient) {
-            stk.add(i);
-            boolean chk = true;
-            while (chk && stk.size() > 3 && stk.peekLast() == 1) {
-                Iterator<Integer> it = stk.descendingIterator();
-                for (int j = 0; j < 4 && it.hasNext() && chk; j++) if (it.next() != bgr[j]) chk = false;
-                if (chk) {
-                    for (int j = 0; j < 4; j++) stk.pollLast();
-                    ++answer;
-                }
-            }
-        }
-        return answer;
-    }
+	private static final int[] bgr = {1, 3, 2, 1}; // burger recipe
+
+	public int solution(int[] ingredient) {
+		int answer = 0;
+		for (int i : ingredient) {
+			stk.add(i);
+			if (stk.size() < 4 || stk.peek() != 1)
+				continue;
+			boolean isBurger = true;
+			for (int j = 0; j < 4; j++) {
+				if (stk.peek() != bgr[j]) {
+					isBurger = false;
+					break;
+				}
+				ttk.push(stk.pop());
+			}
+			if (!isBurger) {
+				while (!ttk.isEmpty())
+					stk.push(ttk.pollFirst());
+			} else {
+				while (!ttk.isEmpty())
+					ttk.pollFirst();
+				++answer;
+			}
+		}
+		return answer;
+	}
 }
