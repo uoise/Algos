ccaad6ca070b36d28630567d1959a093c0492267
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-11-08 14:03:54 +0900

    [Gold III] Title: 리그전 오브 레전드, Time: 132 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Gold/23327. 리그전 오브 레전드/리그전 오브 레전드.cc
+++ 백준/Gold/23327. 리그전 오브 레전드/리그전 오브 레전드.cc
@@ -1,19 +1,16 @@
 #include <cstdio>
-
-int n, v[100001];
-long long pv[100001];
+int n, v[100001], q, s, e;
+long long pv[100001], a, b;
 int main() {
-  int q, s, e;
   scanf("%d %d", &n, &q);
   for (int i = 1; i <= n; i++) {
     scanf("%d", &v[i]);
     pv[i] = v[i] * v[i] + pv[i - 1];
     v[i] += v[i - 1];
   }
-
   while (q--) {
     scanf("%d %d", &s, &e);
-    long long a = pv[e] - pv[s - 1], b = v[e] - v[s - 1];
+    a = pv[e] - pv[s - 1], b = v[e] - v[s - 1];
     printf("%lld\n", (b * b - a) / 2);
   }
 }
