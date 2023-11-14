5efa07e34ff3e31f48a92eb7f30efd6fb0a2db34
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-11-14 16:53:38 +0900

    [Gold II] Title: 철로, Time: 52 ms, Memory: 2788 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.cc
+++ 백준/Gold/13334. 철로/철로.cc
@@ -2,23 +2,25 @@
 #include <cstdio>
 #include <queue>
 
-std::priority_queue<int> q;
-std::pair<int, int> v[100001];
+using namespace std;
+
+priority_queue<int> q;
+pair<int, int> v[100001];
 int n, d, r, s, e;
 int main() {
   scanf("%d", &n);
   for (int i = 0; i < n; i++) {
     scanf("%d %d", &s, &e);
-    v[i].second = std::min(s, e), v[i].first = std::max(s, e);
+    v[i] = {max(s, e), min(s, e)};
   }
   scanf("%d", &d);
-  std::sort(v, v + n);
+  sort(v, v + n);
   for (int i = 0; i < n; i++) {
-    if (v[i].second - v[i].first > d) continue;
-    q.push(-v[i].second);
-    while (!q.empty() && v[i].first + q.top() > d) q.pop();
-    r = std::max(r, (int)q.size());
+    e = v[i].first, s = v[i].second;
+    if (s - e > d) continue;
+    q.push(-s);
+    while (!q.empty() && e + q.top() > d) q.pop();
+    r = max(r, (int)q.size());
   }
-
   printf("%d", r);
 }
