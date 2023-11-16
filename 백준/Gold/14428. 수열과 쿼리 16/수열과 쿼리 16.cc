7a342ad52765358540a806ab75482ae4a970179b
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-11-16 22:17:48 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 92 ms, Memory: 4632 KB -BaekjoonHub

--- 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.cc
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.cc
@@ -1,33 +1,31 @@
-#include <algorithm>
 #include <cstdio>
 
 struct vi {
   int v, i;
-  bool operator<(const vi& o) const { return v == o.v ? i < o.i : v < o.v; }
 };
 
 constexpr int MX = 1e5 + 1, INF = 0x3f3f3f3f;
 int n, vv[MX];
 vi tv[MX * 4];
+vi cmp(const vi& x, const vi& y) { return x.v > y.v ? y : x; }
 vi init(int nod, int beg, int end) {
   if (beg == end) return tv[nod] = {vv[beg], beg + 1};
-  return tv[nod] = std::min(init(nod * 2, beg, (beg + end) / 2),
-                            init(nod * 2 + 1, (beg + end) / 2 + 1, end));
+  return tv[nod] = cmp(init(nod * 2, beg, (beg + end) / 2),
+                       init(nod * 2 + 1, (beg + end) / 2 + 1, end));
 }
 
 vi qry(int nod, int beg, int end, int lft, int rht) {
   if (lft > end || beg > rht) return {INF, INF};
   if (lft <= beg && end <= rht) return tv[nod];
-  return std::min(qry(nod * 2, beg, (beg + end) / 2, lft, rht),
-                  qry(nod * 2 + 1, (beg + end) / 2 + 1, end, lft, rht));
+  return cmp(qry(nod * 2, beg, (beg + end) / 2, lft, rht),
+             qry(nod * 2 + 1, (beg + end) / 2 + 1, end, lft, rht));
 }
 
 vi update(int nod, int beg, int end, int idx, int val) {
   if (idx < beg || idx > end) return tv[nod];
   if (beg == end) return tv[nod] = {val, idx + 1};
-  return tv[nod] =
-             std::min(update(nod * 2, beg, (beg + end) / 2, idx, val),
-                      update(nod * 2 + 1, (beg + end) / 2 + 1, end, idx, val));
+  return tv[nod] = cmp(update(nod * 2, beg, (beg + end) / 2, idx, val),
+                       update(nod * 2 + 1, (beg + end) / 2 + 1, end, idx, val));
 }
 
 int main() {
