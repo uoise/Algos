550167a2be817307e3736fb3064e89e8b31bea6d
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-12-21 15:24:03 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 68 ms, Memory: 4044 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.cc
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.cc
@@ -1,17 +1,22 @@
 #include <algorithm>
 #include <cstdio>
+#include <numeric>
 
 using ll = long long;
 constexpr ll MX = 25e4 + 1;
 int n, c[MX];
-ll v[MX], s;
+ll v[MX];
 ll sol() {
   scanf("%d", &n);
+  ll s = 0, m = -1;
+  int p = -1;
   for (int i = 1; i <= n; i++) {
     scanf("%lld", &v[i]);
     s += v[i];
+    if (v[i] > m) m = v[i], p = i;
   }
   if (!s) return n / 2;
+  std::rotate(v + 1, v + p, v + n + 1);
   for (int i = 1, j; i <= n; i++) {
     if (v[i] || c[i]) continue;
     for (j = i; j <= n && !v[j]; j++) c[j] = 1;
