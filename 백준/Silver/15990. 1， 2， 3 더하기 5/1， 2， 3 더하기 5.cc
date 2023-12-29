dc19560f6e537a09bf03cd55ddfd0d00a7e7c028
youngwoo choi <81301090+uoise@users.noreply.github.com>
2023-12-29 17:34:15 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 0 ms, Memory: 2672 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -0,0 +1,18 @@
+#include <cstdio>
+constexpr int MX = 1e5 + 1, R = 1e9 + 9;
+int r[MX][4];
+int main() {
+  r[1][0] = r[2][1] = 1;
+  r[3][0] = r[3][1] = r[3][2] = 1;
+  for (int i = 4; i < MX; i++) {
+    r[i][0] = (r[i - 1][1] + r[i - 1][2]) % R;
+    r[i][1] = (r[i - 2][0] + r[i - 2][2]) % R;
+    r[i][2] = (r[i - 3][0] + r[i - 3][1]) % R;
+  }
+  int T, n;
+  scanf("%d", &T);
+  while (T--) {
+    scanf("%d", &n);
+    printf("%d\n", ((r[n][0] + r[n][1]) % R + r[n][2]) % R);
+  }
+}
