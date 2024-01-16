0d5cf9494268f156423425490d2bed5c950a3219
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-01-16 21:47:16 +0900

    [Gold V] Title: 등차수열과 쿼리, Time: 740 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.cc
+++ 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.cc
@@ -1,11 +1,13 @@
 #include <cstdio>
-int a, d, q, o, s, e;
-long long sum(int n) { return (n * (2LL * a + (n - 1) * d)) / 2LL; }
-int gcd(int x, int y) { return y ? gcd(y, x % y) : x; }
+using ll = long long;
+int q, o;
+ll a, d, s, e;
+ll sum(ll n) { return (n * (2LL * a + (n - 1) * d)) / 2LL; }
+ll gcd(long x, ll y) { return y ? gcd(y, x % y) : x; }
 int main() {
-  scanf("%d %d %d", &a, &d, &q);
+  scanf("%lld %lld %d", &a, &d, &q);
   while (q--) {
-    scanf("%d %d %d", &o, &s, &e);
+    scanf("%d %lld %lld", &o, &s, &e);
     printf("%lld\n", o == 1 ? sum(e) - sum(s - 1)
                             : (s == e ? a + d * (s - 1) : gcd(a, d)));
   }
