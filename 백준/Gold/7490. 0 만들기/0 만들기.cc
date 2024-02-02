2b094527f7e9af3615cb4efac1fe9972c97883cb
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-02-02 23:03:50 +0900

    [Gold V] Title: 0 만들기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.cc
+++ 백준/Gold/7490. 0 만들기/0 만들기.cc
@@ -1,27 +1,30 @@
 #include <cstdio>
-#include <string>
-
 constexpr char o[] = {' ', '+', '-'};
 int n;
-void btr(int c, std::string s) {
+char s[20] = {'1',};
+void btr(int c) {
   if (c == n) {
     int r = 0, v = 0, p = 1;
-    for (const char c : s) {
-      if (c == ' ') continue;
-      if (c == '+' || c == '-') r += v * p, v = 0, p = c == '+' ? 1 : -1;
-      else v = v * 10 + c - '0';
+    for (int i = 0; i < n * 2 - 1; i++) {
+      if (s[i] == ' ') continue;
+      if (s[i] < '0') r += v * p, v = 0, p = s[i] == '+' ? 1 : -1;
+      else v = v * 10 + s[i] - '0';
     }
     r += v * p;
-    if (!r) printf("%s\n", s.c_str());
+    if (!r) printf("%s\n", s);
     return;
   }
 
-  for (int i = 0; i < 3; i++) btr(c + 1, s + o[i] + (char)('0' + c + 1));
+  for (int i = 0; i < 3; i++) {
+    s[c * 2 - 1] = o[i], s[c * 2] = '0' + c + 1;
+    btr(c + 1);
+  }
 }
 
 void sol() {
   scanf("%d", &n);
-  btr(1, "1");
+  s[0] = '1';
+  btr(1);
   printf("\n");
 }
 
