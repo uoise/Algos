b7a9073d9abcde59ad371f2b27f49c504d72a301
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-02-19 18:45:11 +0900

    [Gold V] Title: 빌런 호석, Time: 44 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.cc
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.cc
@@ -2,14 +2,13 @@
 #include <cstdio>
 #include <string>
 
-constexpr std::bitset<7> sv[10] = {0b1111110, 0b0110000, 0b1101101, 0b1111001,
-                                   0b0110011, 0b1011011, 0b1011111, 0b1110000,
-                                   0b1111111, 0b1111011};
+constexpr std::bitset<7> sv[10] = {0b1111110, 0b0110000, 0b1101101, 0b1111001, 0b0110011, 
+                                   0b1011011, 0b1011111, 0b1110000, 0b1111111, 0b1111011};
 std::string c, a;
 int n, k, p;
-bool clc(int tgt) {
+bool clc(int x) {
   int cnt = 0;
-  a = std::to_string(tgt);
+  a = std::to_string(x);
   while (a.length() < k) a = '0' + a;
   for (int i = 0; i < k; i++) cnt += (sv[c[i] - '0'] ^ sv[a[i] - '0']).count();
   return p >= cnt;
@@ -20,7 +19,7 @@ int sol() {
   scanf("%d %d %d %d", &n, &k, &p, &x);
   c = std::to_string(x);
   while (c.length() < k) c = '0' + c;
-  for (int i = 1; i <= n; i++) r += clc(i);
+  while (n) r += clc(n--);
   return r;
 }
 
