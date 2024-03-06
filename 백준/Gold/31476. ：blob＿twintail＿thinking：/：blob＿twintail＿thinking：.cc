309c4b37315451b18f01381bc6c2251c9c31c199
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-03-07 00:31:56 +0900

    [Gold V] Title: :blob_twintail_thinking:, Time: 0 ms, Memory: 1132 KB -BaekjoonHub

--- 백준/Gold/31476. ：blob＿twintail＿thinking：/：blob＿twintail＿thinking：.cc
+++ 백준/Gold/31476. ：blob＿twintail＿thinking：/：blob＿twintail＿thinking：.cc
@@ -1,26 +1,24 @@
 #include <algorithm>
 #include <cstdio>
 
-constexpr int MX = (1 << 12) - 1;
-bool ev[MX][MX];
-long long tr, pr;
-int d, u, t, l;
-bool vld(int s, int e) { return (e < (1 << d)) && (!ev[s][e]); }
+long long u, t, tr, pr;
+int d, ev[4095], l;
+bool vld(int s, int e) { return (e < (1 << d)) && (ev[e] != s); }
 void dfs(int c, int k, long long s) {
   int a = c * 2;
   int b = vld(c, a) && vld(c, a + 1);
   tr = std::max(tr, s), l = c;
-  if (vld(c, a)) dfs(a, k + b, s + u + (k + b) * t), pr += 2LL * u;
-  if (vld(c, ++a)) dfs(a, k + b, s + u + (k + b) * t), pr += 2LL * u;
+  if (vld(c, a)) dfs(a, k + b, s + u + (k + b) * t), pr += u * 2;
+  if (vld(c, ++a)) dfs(a, k + b, s + u + (k + b) * t), pr += u * 2;
 }
 
 int main() {
   int n;
-  scanf("%d %d %d %d", &d, &n, &u, &t);
+  scanf("%d %d %lld %lld", &d, &n, &u, &t);
   while (n--) {
     int s, e;
     scanf("%d %d", &s, &e);
-    ev[s][e] = ev[e][s] = 1;
+    ev[e] = s;
   }
   dfs(1, 0, 0);
   while (l != 1) pr -= u, l /= 2;
