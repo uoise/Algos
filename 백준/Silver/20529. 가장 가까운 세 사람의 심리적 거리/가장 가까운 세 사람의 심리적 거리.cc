03f7e793cdc644e4a96dc707330a1a48339dc9d8
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-03-08 20:54:37 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 16 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -4,23 +4,24 @@
 
 constexpr char MB[] = {'E', 'S', 'T', 'J'};
 char inp[5];
-std::bitset<4> v[100001];
+std::bitset<4> v[256];
 int sol() {
   int n, r = 99;
   scanf("%d\n", &n);
   for (int i = 0; i < n; i++) {
     scanf("%s", inp);
-    for (int j = 0; j < 4; j++) v[i][j] = inp[j] == MB[j];
+    for (int j = 0; n <= 256 && j < 4; j++) v[i][j] = inp[j] == MB[j];
   }
 
-  if (n > 16 * 16) return 0;
+  if (n > 256) return 0;
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       if (i == j) continue;
       for (int k = 0; k < n; k++) {
         if (i != k && j != k)
-          r = std::min(r, (int)((v[i] ^ v[j]).count() + (v[j] ^ v[k]).count() + (v[k] ^ v[i]).count()));
+          r = std::min(r, (int)((v[i] ^ v[j]).count() + (v[j] ^ v[k]).count() +
+                                (v[k] ^ v[i]).count()));
       }
     }
   }
