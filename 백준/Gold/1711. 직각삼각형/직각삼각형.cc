3dbb95dcddbf7ae45fc314337bd80903f3590b97
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-03-28 20:15:08 +0900

    [Gold V] Title: 직각삼각형, Time: 4120 ms, Memory: 18736 KB -BaekjoonHub

--- 백준/Gold/1711. 직각삼각형/직각삼각형.cc
+++ 백준/Gold/1711. 직각삼각형/직각삼각형.cc
@@ -1,24 +1,25 @@
+#include <algorithm>
 #include <cstdio>
 
 int n, r;
 long long v[1501][2], l[1501][1501];
-long long len(int a, int b) {
+long long len(const int& a, const int& b) {
   if (l[a][b]) return l[a][b];
   return l[a][b] = (v[a][0] - v[b][0]) * (v[a][0] - v[b][0]) +
                    (v[a][1] - v[b][1]) * (v[a][1] - v[b][1]);
 }
 
-bool sol(int a, int b, int c) {
-  auto x = len(a, b), y = len(b, c), z = len(a, c);
+bool sol(const int& a, const int& b, const int& c) {
+  const auto x = len(a, b), y = len(b, c), z = len(a, c);
   return (x + y == z) || (y + z == x) || (z + x == y);
 }
 
 int main() {
   scanf("%d", &n);
-  for (int i = 0; i < n; i++) scanf("%lld %lld", &v[i][0], &v[i][1]);
-  for (int i = 0; i < n; i++)
-    for (int j = i + 1; j < n; j++)
-      for (int k = j + 1; k < n; k++)
+  for (register int i = 0; i < n; i++) scanf("%lld %lld", &v[i][0], &v[i][1]);
+  for (register int i = 0; i < n; i++)
+    for (register int j = i + 1; j < n; j++)
+      for (register int k = j + 1; k < n; k++)
         if (sol(i, j, k)) ++r;
   printf("%d", r);
 }
