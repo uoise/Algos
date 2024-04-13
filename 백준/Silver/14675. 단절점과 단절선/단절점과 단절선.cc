2f19514332aab109f25dcc654a2b44788e175364
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-04-13 23:50:47 +0900

    [Silver I] Title: 단절점과 단절선, Time: 52 ms, Memory: 1500 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
@@ -1,23 +1,18 @@
 #include <cstdio>
-#include <vector>
 
-constexpr int MN = 1e5 + 1;
-int n, c[MN];
-bool cuv(int x) { return c[x] > 1; }
-bool sol(int t, int K) { return t == 1 ? cuv(K) : true; }
+int c[100001];
 int main() {
+  int n, s, e;
   scanf("%d", &n);
   for (int i = 1; i < n; i++) {
-    int s, e;
     scanf("%d %d", &s, &e);
     ++c[s], ++c[e];
   }
 
-  int q;
-  scanf("%d", &q);
-  while (q--) {
-    int t, K;
-    scanf("%d %d", &t, &K);
-    printf("%s\n", sol(t, K) ? "yes" : "no");
+  scanf("%d", &n);
+  while (n--) {
+    int s, e;
+    scanf("%d %d", &s, &e);
+    printf("%s\n", (s == 1 ? c[e] > 1 : true) ? "yes" : "no");
   }
 }
