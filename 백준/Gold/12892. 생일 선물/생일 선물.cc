28e4582c4ac761605924b3d6bdfd4725167c3dca
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-04-24 21:29:28 +0900

    [Gold IV] Title: 생일 선물, Time: 44 ms, Memory: 1896 KB -BaekjoonHub

--- 백준/Gold/12892. 생일 선물/생일 선물.cc
+++ 백준/Gold/12892. 생일 선물/생일 선물.cc
@@ -3,14 +3,13 @@
 
 std::pair<int, int> v[100001];
 long long r, c;
-int n, d, s, e, t;
+int n, d, s, e;
 int main() {
   scanf("%d %d", &n, &d);
   for (int i = 0; i < n; i++) scanf("%d %d", &v[i].first, &v[i].second);
   std::sort(v, v + n);
   while (s < n && e < n) {
-    t = v[e].first - v[s].first;
-    if (t < d) {
+    if (v[e].first - v[s].first < d) {
       c += v[e++].second;
       r = std::max(r, c);
     } else {
