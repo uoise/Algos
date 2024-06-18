20905724c4e57100569c70dfaae8bd5957167760
youngwoo choi <81301090+uoise@users.noreply.github.com>
2024-06-18 21:03:13 +0900

    [Gold V] Title: 두 배, Time: 48 ms, Memory: 3064 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -1,21 +1,29 @@
-#include <cmath>
 #include <cstdio>
 
-double a[250001];
-int n, r;
+constexpr int MN = 25e4 + 1;
+long long r;
+int n, a[MN], v[MN];
+int sol(long long x, long long y) {
+  x <<= 20;
+  for (int i = -20; i < 0; i++) {
+    if (x <= y) return i;
+    x >>= 1;
+  }
+  for (int i = 0; i < 20; i++) {
+    if (x <= y) return i;
+    y <<= 1;
+  }
+  return 20;
+}
+
 int main() {
   scanf("%d", &n);
   for (int i = 0; i < n; i++) {
-    int x;
-    scanf("%d", &x);
-    a[i] = log2(x);
+    scanf("%d", &a[i]);
+    if (i) v[i] = sol(a[i - 1], a[i]);
   }
 
-  for (int i = 1; i < n; i++) {
-    if (a[i] >= a[i - 1]) continue;
-    int t = ceil(a[i - 1] - a[i]);
-    r += t, a[i] += t;
-  }
+  for (int i = 1; i < n; i++) if (v[i] > 0) r += v[i], v[i + 1] += v[i];
 
-  printf("%d", r);
+  printf("%lld", r);
 }
